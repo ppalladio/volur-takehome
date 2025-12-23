62a83d0e37535e8fe9a77a9b9e72d7f0cb398b81
Yuxuan <nine.slide@gmail.com>
2025-12-23 18:00:40 +0100

    feat: update layout pages

--- src/app/editor/components/BlockList.tsx
+++ src/app/editor/components/BlockList.tsx
@@ -1,28 +1,20 @@
-"use client";
+'use client';
 
-import { Block, Path } from "@/lib";
-import BlockNode from "./BlockNode";
+import { Block, Path } from '@/lib/editor/types';
+import BlockNode from './BlockNode';
 
 type BlockListProps = {
-  blocks: Block[];
-  parentPath: Path | null;
+    blocks: Block[];
+    parentPath: Path | null;
 };
 
 export default function BlockList({ blocks, parentPath }: BlockListProps) {
-  return (
-    <div className="space-y-1">
-      {blocks.map((block, index) => {
-        const path = parentPath ? [...parentPath, index] : [index];
-        return (
-          <BlockNode
-            key={block.id}
-            block={block}
-            path={path}
-            index={index}
-            parentPath={parentPath}
-          />
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className="space-y-1">
+            {blocks.map((block, index) => {
+                const path = parentPath ? [...parentPath, index] : [index];
+                return <BlockNode key={block.id} block={block} path={path} index={index} parentPath={parentPath} />;
+            })}
+        </div>
+    );
 }
